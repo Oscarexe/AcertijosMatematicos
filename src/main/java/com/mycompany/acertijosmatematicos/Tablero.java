38cfcdfd1b3dae471fff4836bebdb8b7919163aa
Oscar <Oscar@192.168.15.213>
2020-03-03 14:52:36 +0100

    empezando la lógica del juego

--- src/main/java/com/mycompany/acertijosmatematicos/Tablero.java
+++ src/main/java/com/mycompany/acertijosmatematicos/Tablero.java
@@ -16,7 +16,7 @@ public class Tablero extends Pane {
         //int 
         for(int i=0; i<anchoTablero+ 2; i++) {
            // Line line = new Line(Cuadrante.TAM_CUADRANTE, Cuadrante.TAM_CUADRANTE*i, Cuadrante.TAM_CUADRANTE*7, Cuadrante.TAM_CUADRANTE*i);
-            Line line = new Line(Carta.TAM_CUADRANTE*i, Carta.TAM_CUADRANTE, Carta.TAM_CUADRANTE*i, Carta.TAM_CUADRANTE*altoTablero+2);
+            Line line = new Line(Carta.TAM_CUADRANTE*i, Carta.TAM_CUADRANTE, Carta.TAM_CUADRANTE*i, Carta.TAM_CUADRANTE*(altoTablero+1));
            // this.getChildren().add(line);
             this.getChildren().add(line);
             
@@ -24,7 +24,7 @@ public class Tablero extends Pane {
         }
         // lineas Horizontales
       for(int i=0; i<altoTablero+ 2; i++) {
-            Line line = new Line(Carta.TAM_CUADRANTE, Carta.TAM_CUADRANTE*i, Carta.TAM_CUADRANTE*altoTablero+2, Carta.TAM_CUADRANTE*i);
+            Line line = new Line(Carta.TAM_CUADRANTE, Carta.TAM_CUADRANTE*i, Carta.TAM_CUADRANTE*(anchoTablero+1), Carta.TAM_CUADRANTE*i);
             this.getChildren().add(line);
     
         }
@@ -40,6 +40,7 @@ public class Tablero extends Pane {
                 carta.setLayoutX(Carta.TAM_CUADRANTE*e+ Carta.TAM_CUADRANTE);
                 carta.setLayoutY(Carta.TAM_CUADRANTE*i+ Carta.TAM_CUADRANTE);
                 this.getChildren().add(carta);
+                
             }
         }    
     }
